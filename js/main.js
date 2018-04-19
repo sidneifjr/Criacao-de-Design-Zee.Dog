0eb0f6a4e76df7cb273d790b84eaceb306187b15
sidneifjr <sidneijr.77@gmail.com>
2018-04-18 23:14:19 -0300

    Released too!

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var modal = document.getElementById('modal'),
+    btn = document.getElementById('btn');
+
+btn.addEventListener("click", function(){
+  modal.classList.add('active');
+  
+  window.setTimeout(function(){
+    modal.style.display = 'none';
+  }, 3000);
+});
